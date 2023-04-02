a199d724a5a426c2ee89393a1d0406c323e9e5d7
jrfm <moesch.jonas@protonmail.com>
2023-04-02 17:56:40 +0200

    add basic header component2

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import App from "./App";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div id="header--container">
+      <h1>Structures</h1>
+      <ul id="link--container">
+        <li>
+          <Link to="/" style={{ textDecoration: "none", color: "black" }}>
+            Home
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="" style={{ textDecoration: "none", color: "black" }}>
+            Products
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="" style={{ textDecoration: "none", color: "black" }}>
+            Shopping Cart
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
